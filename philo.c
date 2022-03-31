bd0e273acdc2501ae14c328e1b93e7e92d2ef5fd
Maria Daan <mariadaan@gmail.com>
2022-03-31 17:24:20 +0200

    practice

--- philo.c
+++ philo.c
@@ -16,18 +16,27 @@ void *routine()
 	}
 	printf("%d ", global);
 	pthread_mutex_unlock(&mutex);
+	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
+	// to do: create array of threads, variable length
 	pthread_t t1;
 	pthread_t t2;
 
+	// The pthread_mutex_init() function creates a new mutex
 	pthread_mutex_init(&mutex, NULL);
-	pthread_create(&t1, NULL, &routine, NULL);
-	pthread_create(&t2, NULL, &routine, NULL);
+
+	// create threads
+	pthread_create(&t1, NULL, routine, NULL);
+	pthread_create(&t2, NULL, routine, NULL);
+
+	// wait
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+
+	//frees the resources allocated for mutex
 	pthread_mutex_destroy(&mutex);
 
 	return (0);
