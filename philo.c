fe57b0c5a7b7a400a6213b34d27ac227fcaf500b
Maria Daan <mariadaan@gmail.com>
2022-05-04 16:00:30 +0200

    lekker gekloot

--- philo.c
+++ philo.c
@@ -2,42 +2,123 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdbool.h>
+#include "philo.h"
 
-int global = 0;
-pthread_mutex_t mutex;
+// int number_of_philosophers = 3;
+// int time_to_die = 200;
+// int time_to_eat = 100;
+// int time_to_sleep = 150;
 
 
-void *routine()
-{
-	pthread_mutex_lock(&mutex);
-	for (int i = 0; i < 1000000; i++){
-		global++;
+// void *routine()
+// {
+// 	pthread_mutex_lock(&mutex);
+// 	for (int i = 0; i < 1000000; i++){
+// 		global++;
 		
-	}
-	printf("%d ", global);
-	pthread_mutex_unlock(&mutex);
+// 	}
+// 	printf("%d ", global);
+// 	pthread_mutex_unlock(&mutex);
+// 	return NULL;
+// }
+
+void create_new_philo(t_philo *philo)
+{
+	philo->l_fork = false;
+	philo->r_fork = false;
+	philo->eating = false;
+	philo->sleeping = false;
+	philo->dead = false;
+}
+
+void *routine(void *arguments){
+	// philosopher comes to life
+	t_philo philo;
+
+	create_new_philo(&philo);
+	int index = *((int *)arguments);
+	int sleep_time = ((int *)arguments)[1];
+	printf("THREAD %d: Started.\n", index);
+	printf("THREAD %d: Will be sleeping for %d seconds.\n", index, sleep_time);
+	// while !dead (timestamp < timetodie)
+		// eat
+		// sleep(time_to_eat);
+		// put forks back
+		sleep(sleep_time);
+		// find fork
+			// loop until fork is found and !dead
+
+
+	// philosopher dies
+	printf("THREAD %d: Ended.\n", index);
 	return NULL;
+}
+
+
+void get_args(t_args *args, char *argv[])
+{
+
 }
 
 int main(int argc, char *argv[])
 {
-	// to do: create array of threads, variable length
-	pthread_t t1;
-	pthread_t t2;
+	t_args args;
 
-	// The pthread_mutex_init() function creates a new mutex
-	pthread_mutex_init(&mutex, NULL);
+	if (check_input(argc, argv))
+		printf("expected usage: ./philo 200 100 150");
+	exit(0);
+	get_args(&args, argv);
+	// create amount of philosophers threads
+	int number_of_philosophers = atoi(argv[1]);
+	printf("num: %d\n", number_of_philosophers);
+	pthread_t threads[number_of_philosophers];
 
-	// create threads
-	pthread_create(&t1, NULL, routine, NULL);
-	pthread_create(&t2, NULL, routine, NULL);
+	// save
+	int time_to_die =  atoi(argv[2]);
+	int time_to_eat =  atoi(argv[3]);
+	int time_to_sleep =  atoi(argv[4]);
 
-	// wait
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
+	int *thread_args = malloc(5 * sizeof(int));
+	// thread_args[0] = number_of_philosophers;
+	thread_args[1] = time_to_die;
+	thread_args[2] = time_to_eat;
+	thread_args[3] = time_to_sleep;
+	thread_args[4] = 0;
 
-	//frees the resources allocated for mutex
-	pthread_mutex_destroy(&mutex);
+	int result_code;
+
+	int philo_index = 0;
+	while (philo_index < number_of_philosophers)
+	{
+		thread_args[0] = philo_index;
+		printf("IN MAIN: Creating thread %d.\n", philo_index);
+		result_code = pthread_create(&threads[philo_index], NULL, routine, thread_args);
+		if (result_code)
+		{
+			printf("exit\n");
+			exit(0);
+		}
+		philo_index++;
+	}
+
+	printf("IN MAIN: All threads are created.\n");
+
+
+
+	// // The pthread_mutex_init() function creates a new mutex
+	// pthread_mutex_init(&mutex, NULL);
+
+	// // create threads
+	// pthread_create(&t1, NULL, routine, NULL);
+	// pthread_create(&t2, NULL, routine, NULL);
+
+	// // wait
+	// pthread_join(t1, NULL);
+	// pthread_join(t2, NULL);
+
+	// //frees the resources allocated for mutex
+	// pthread_mutex_destroy(&mutex);
 
 	return (0);
 }
