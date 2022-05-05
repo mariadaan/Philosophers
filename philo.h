4735e2e1149ab6463114818f6d7e07d47c3e781c
Maria Daan <mariadaan@gmail.com>
2022-05-05 17:50:47 +0200

    new funcs

--- philo.h
+++ philo.h
@@ -17,19 +17,23 @@ typedef struct s_philo
 
 typedef struct s_args
 {
-	int		number_of_philosophers;
+	int		num_philo;
 	int		time_to_die;
 	int		time_to_eat;
 	int		time_to_sleep;
 	bool	*forks;
 }		t_args;
 
+
+// let every fork be a mutex itself, not an int protected by a mutex
 typedef struct s_fork
 {
 	bool	fork_status;
 	// mutex;
 }	t_fork;
 
-int	check_input(int argc, char **input);
+int		check_input(int argc, char **input);
+int		ft_strlen(const char *s);
+int		ft_atoi(const char *str);
 
 #endif
